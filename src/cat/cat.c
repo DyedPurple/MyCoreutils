f14e9d094ba32c71b3ee9f083f2a78171ce2b114
George <dyedpurple@googlemail.com>
2016-04-01 23:49:05 +0100

    Moved stuff into functions, and half implemented return codes

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -10,18 +10,50 @@ void print_usage(char *prog_location) {
 	exit(EXIT_FAILURE);
 }
 
+int cat_from_stdin() {
+
+	char c;			/* Char being read from file */
+
+	while ((c = getchar()) != EOF) {
+		putchar(c);
+	}
+
+	return 0;
+}
+
+int cat_file(FILE *fp) {
+
+	char c;			/* Char being read from file */
+	char prev_c;	/* Previous char read from file */
+
+	c = getc(fp);
+
+	if (c == EOF) {
+		return 0;
+	}
+
+	while (c != EOF) {
+		putchar(c);
+		prev_c = c;
+		c = getc(fp);
+	}
+
+	if (prev_c != '\n') {
+		putchar('\n');
+	}
+
+	return 0;
+}
+
 int main(int argc, char **argv) {
 
 	FILE *fp;
 	int arg_i;		/* Argument parser pointer */
-	char c;			/* Char being read from file */
-	char prev_c;	/* Previous char read from file */
+	int return_code = 0; /* Default to returning success */
 
 	/* Read input from stdin */
 	if (argc == 1) {
-		while ((c = getchar()) != EOF) {
-			putchar(c);
-		}
+		return_code = cat_from_stdin();
 	}
 
 	/* Read input from files */
@@ -34,21 +66,7 @@ int main(int argc, char **argv) {
 				continue;
 			}
 
-			c = getc(fp);
-
-			if (c == EOF) {
-				continue;
-			}
-
-			while (c != EOF) {
-				putchar(c);
-				prev_c = c;
-				c = getc(fp);
-			}
-
-			if (prev_c != '\n') {
-				putchar('\n');
-			}
+			return_code = cat_file(fp);
 
 			if (fclose(fp) == EOF) {
 				fprintf(stderr, "WARNING: Failed to close %s. Continuing...\n", argv[arg_i]); 
@@ -56,5 +74,5 @@ int main(int argc, char **argv) {
 		}
 	}
 	
-	return 0;
+	return return_code;
 }
--- src/cat/cat.h
+++ src/cat/cat.h
@@ -1,2 +1,4 @@
 #define GH_CAT_H
 void print_usage(char *prog_location);
+int cat_from_stdin();
+int cat_file(FILE *fp);
